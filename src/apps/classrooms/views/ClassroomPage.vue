78a64bf226166ff72f75cb34b2742079d9c3d379
Raj Tagore <rajtagore780@gmail.com>
2025-02-25 19:13:57 +0530

    grid for cards and Datalist component exp

--- src/apps/classrooms/views/ClassroomPage.vue
+++ src/apps/classrooms/views/ClassroomPage.vue
@@ -1,38 +1,57 @@
 <template>
-	<v-container>
-		<v-row align="center" justify="center" v-if="classroom">
-			<v-col>
-				<v-row class="ma-2">
-					<v-col cols="12" lg="4">
-						<ClassroomCard :classroomId="classroomId" />
-					</v-col>
-					<v-col cols="12" lg="4">
-						<SubjectsList :filter="{ classroom: classroom.id }" />
-					</v-col>
-					<v-col cols="12" lg="4">
-						<AnnouncementsList :filter="{ classroom: classroom.id }" title="Announcements" :to="`ClassroomAnnouncements`" />
-					</v-col>
-				</v-row>
-			</v-col>
-		</v-row>
+	<v-container class="columns-container" v-if="classroom">
+		<ClassroomCard class="column-item" :classroomId="classroomId" />
+		<SubjectsList class="column-item" :filter="{ classroom: classroom.id }" />
+		<AnnouncementsDataList
+			class="column-item"
+			title="Announcements"
+			:filters="{ classroom: classroom.id }"
+			:buttons="[
+				{
+					text: 'View All Announcements',
+					to: { name: 'ClassroomAnnouncements', params: { classroomId } }
+				}
+			]"
+		/>
 	</v-container>
 </template>
 
 <script setup>
-import { getClassroom } from "@/apps/classrooms/api";
 import { ref } from "vue";
-
-const classroom = ref(null);
+import { getClassroom } from "@/apps/classrooms/api";
 
 const props = defineProps({
 	classroomId: Number,
 });
 
-import AnnouncementsList from "@/apps/announcements/components/AnnouncementsList.vue";
-import AnnouncementsLookup from "@/apps/announcements/components/AnnouncementsLookup.vue";
-import AssignmentsLookup from "@/apps/assignments/components/AssignmentsLookup.vue";
+const classroom = ref(null);
+
 import ClassroomCard from "@/apps/classrooms/components/ClassroomCard.vue";
 import SubjectsList from "@/apps/subjects/components/SubjectsList.vue";
+import AnnouncementsDataList from "@/apps/announcements/components/AnnouncementsDataList.vue";
 
+// Fetch classroom data
 classroom.value = await getClassroom(props.classroomId);
 </script>
+
+<style scoped>
+.columns-container {
+	/* Default: one column (for mobile) */
+	column-count: 1;
+	column-gap: 1rem;
+	/* You may need to adjust padding or margins inherited from v-container */
+}
+
+/* For larger screens, use 3 columns */
+@media (min-width: 1024px) {
+	.columns-container {
+		column-count: 3;
+	}
+}
+
+/* Prevent cards from splitting between columns */
+.column-item {
+	break-inside: avoid;
+	margin-bottom: 1rem;
+}
+</style>
--- src/components/FormCard.vue
+++ src/components/FormCard.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-card>
 		<v-card-title>
-			{{actionName}} {{ title }}
+			{{ actionName }} {{ title }}
 		</v-card-title>
 		<v-card-text>
 			<v-row>
