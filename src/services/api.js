13c9c91601966de21ab2705df05ce237630197cc
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-02-25 16:55:26 +0530

    Changing the base url for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import { useAuthStore } from "@/stores/auth";
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://school1.localhost:8000/",
+	baseURL: "",
 	headers: {
 		"Content-Type": "application/json",
 	},
@@ -54,7 +54,7 @@ api.interceptors.response.use(
 			if (refresh) {
 				try {
 					const response = await axios.post(
-						"http://school1.localhost:8000/api/token/refresh/",
+						"/api/token/refresh/",
 						{ refresh: refresh },
 					);
 					authStore.refreshTokens({
