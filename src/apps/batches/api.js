1f2739e5f68982f3e5ba87e79196ae42341da591
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-03-13 18:18:12 +0530

    Images now load in batches

--- src/apps/batches/api.js
+++ src/apps/batches/api.js
@@ -1,14 +1,21 @@
 import { createViewset } from "@/services/viewset";
+import { getRandomImage } from "@/services/api";
 
-const images = import.meta.glob("@/assets/batches/batch*.png");
+const images = Object.values(
+    import.meta.glob("@/assets/batches/batch*.png", {
+        eager: true,
+        query: "?url",
+    }),
+);
 
 const batchViewset = createViewset("api/allocation/batches");
 
 // Get base methods
-const getBatches = (filter = {}) => batchViewset.list({
-	page_size: 100,
-	...filter
-});
+const getBatches = (filter = {}) =>
+    batchViewset.list({
+        page_size: 100,
+        ...filter,
+    });
 const getBatch = batchViewset.retrieve;
 const updateBatch = batchViewset.update;
 const createBatch = batchViewset.create;
@@ -17,25 +24,22 @@ const importBatchesDryRun = batchViewset.import.dryRun;
 const importBatchesFinalize = batchViewset.import.finalize;
 
 // Custom methods
-const getBatchImage = () => {
-	const index = Math.floor(Math.random() * images.length);
-	return images[index];
-};
+const getBatchImage = () => getRandomImage(images);
 
 const getBatchInfoFromObj = (item) => ({
-	title: item.name,
-	subtitle: `Grade ${item.standard}`,
-	value: item.id,
+    title: item.name,
+    subtitle: `Grade ${item.standard}`,
+    value: item.id,
 });
 
 export {
-	getBatch,
-	getBatches,
-	updateBatch,
-	getBatchImage,
-	getBatchInfoFromObj,
-	createBatch,
-	exportBatches,
-	importBatchesDryRun,
-	importBatchesFinalize,
+    getBatch,
+    getBatches,
+    updateBatch,
+    getBatchImage,
+    getBatchInfoFromObj,
+    createBatch,
+    exportBatches,
+    importBatchesDryRun,
+    importBatchesFinalize,
 };
