59387b1153972608e708cf6a99b8c9b17187615d
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-03-15 17:23:50 +0530

    Nothing changed, just pulling

--- src/apps/attachments/api.js
+++ src/apps/attachments/api.js
@@ -1,18 +1,21 @@
 import { api } from "@/services/api";
+import { createViewset } from "@/services/viewset";
+
+const attachmentViewset = createViewset("api/attachments");
 
 const createAttachment = async (formObj) => {
-	const data = new FormData();
-	for (const [key, value] of Object.entries(formObj)) {
-		data.append(key, value);
-	}
-	return (
-		await api.post("api/attachments/create/", data, {
-			headers: { "Content-Type": "multipart/form-data" },
-		})
-	).data;
+    const data = new FormData();
+    for (const [key, value] of Object.entries(formObj)) {
+        data.append(key, value);
+    }
+    return (
+        await api.post("api/attachments/create/", data, {
+            headers: { "Content-Type": "multipart/form-data" },
+        })
+    ).data;
 };
 
-const getAttachments = async (filters) =>
-	(await api.post("api/attachments/all", filters)).data;
+const getAttachments = attachmentViewset.list;
+const getAttachment = attachmentViewset.retrieve;
 
-export { createAttachment, getAttachments };
+export { createAttachment, getAttachments, getAttachment };
---
+++ src/apps/attachments/views/AttachmentsPage.vue
@@ -0,0 +1,28 @@
+<template>
+	<FilterCard></FilterCard>
+</template>
+
+<script setup>
+import FilterCard from "@/components/FilterCard.vue";
+import ResponsiveDataTable from "@/components/ResponsiveDataTable.vue";
+
+import { computed, ref } from "vue";
+
+const defaultFields = [
+	{
+		label: "Search by name",
+		type: "string",
+		key: "name",
+		value: "",
+		defaultValue: "",
+	},
+];
+
+const filters = computed(() =>
+	defaultFields.reduce((acc, e) => {
+		acc[e.key] = e.value;
+		return acc;
+	}, {}),
+);
+
+</script>
