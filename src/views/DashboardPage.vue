7141e66801d2aef671235b7927c87cc8ee10bdfa
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-03-17 10:36:24 +0530

    Using StatsCard in Dashboard

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-container class="columns-container">
-		<StudentsStatsCard class="column-item" />
-		<TeachersStatsCard class="column-item" />
+		<StatsCard class="column-item" title="Students" :fetchStats="getStudentStats" />
+		<StatsCard class="column-item" title="Teachers" :fetchStats="getTeacherStats" />
 		<GenericList 
 			class="column-item"
 			:fetchFunction="getAnnouncements"
@@ -13,9 +13,11 @@
 </template>
 
 <script setup>
-import StudentsStatsCard from "@/apps/students/components/StudentsStatsCard.vue";
-import TeachersStatsCard from "@/apps/teachers/components/TeachersStatsCard.vue";
+import { getStudentStats } from "@/apps/students/api";
+import { getTeacherStats } from "@/apps/teachers/api";
+
 import { getAnnouncements } from "@/apps/announcements/api";
 import { announcementListConfig } from "@/apps/announcements/config";
 import GenericList from "@/components/GenericList.vue";
+import StatsCard from "@/components/StatsCard.vue";
 </script>
