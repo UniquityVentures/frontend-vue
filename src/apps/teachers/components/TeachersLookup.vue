0c4a1517e1ef6a9b46da00e55ff11ab3ee248cd1
Raj Tagore <rajtagore780@gmail.com>
2025-03-24 06:33:48 +0530

    --

--- src/apps/teachers/components/TeachersLookup.vue
+++ src/apps/teachers/components/TeachersLookup.vue
@@ -1,6 +1,6 @@
 <template>
 	<ResponsiveDataTable :fetch="getTeachers" v-model:filters="filters"
-		title="Teachers" subtitle="Teachers Master List">
+		title="Teachers" subtitle="Teachers Master List" :templates="{ desktop: 'card', mobile: 'card' }">
 		<template #filters-slot>
 			<v-row>
 				<v-col cols="12" sm="6" md="3" lg="2">
@@ -14,18 +14,23 @@
 				</v-col>
 			</v-row>
 		</template>
-		<template #list-slot="{ items }">
-			<v-list lines="two">
-				<v-list-item v-for="item in items" :key="item.id" link 
-					:to="{ name: 'Teacher', params: { teacherId: item.id }}" class="border">
-					<v-list-item-title>{{ item.user_details.full_name }}</v-list-item-title>
-					<v-list-item-subtitle>{{ item.identifier }}</v-list-item-subtitle>
-					<v-list-item-text>
-						<v-chip color="green">{{ item.phone }}</v-chip>
-						<v-chip color="blue">{{ item.user_details.email }}</v-chip>
-					</v-list-item-text>
-				</v-list-item>
-			</v-list>
+		<template #cards-slot="{ items }">
+			<v-row>
+				<v-col cols="12" md="4" lg="3" v-for="item in items" :key="item.id">
+					<v-card height="100%" link :to="{ name: 'Teacher', params: { teacherId: item.id }}" variant="flat" class="border">
+						<v-card-title class="text-subtitle-1">{{ item.user_details.full_name }}</v-card-title>
+						<v-card-subtitle>{{ item.identifier }}</v-card-subtitle>
+						<v-card-text>
+							<v-chip prepend-icon="mdi-email" color="primary">
+								{{ item.user_details.email }}
+							</v-chip><br>
+							<v-chip prepend-icon="mdi-whatsapp" color="green">
+								{{ item.whatsapp }}
+							</v-chip>
+						</v-card-text>
+					</v-card>
+				</v-col>
+			</v-row>
 		</template>
 	</ResponsiveDataTable>
 </template>
@@ -36,6 +41,5 @@ import { getTeachers } from "../api";
 import ResponsiveDataTable from "@/components/ResponsiveDataTable.vue";
 import BatchSelect from "@/apps/batches/components/BatchSelect.vue";
 import CourseSelect from "@/apps/courses/components/CourseSelect.vue";
-
 const filters = ref({});
 </script>
