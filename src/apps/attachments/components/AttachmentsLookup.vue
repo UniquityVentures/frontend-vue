b18083faa17f191228ddfe6c7062861fcdb1b642
Raj Tagore <rajtagore780@gmail.com>
2025-03-30 05:04:43 +0530

    attachments lookup. also removed the create attachment page.

---
+++ src/apps/attachments/components/AttachmentsLookup.vue
@@ -0,0 +1,72 @@
+<template>
+	<ResponsiveDataTable 
+		:getToFunction="(item) => ({ name: 'Attachment', params: { attachmentId: item.id } })"
+		:headers="attachmentDefaultHeaders" 
+		:fetch="getAttachments" 
+		v-model:filters="filters" 
+		:templates="{ desktop: 'card', mobile: 'card' }"
+		:title="title"
+		:subtitle="subtitle">
+		
+		<template #filters-slot>
+			<v-row>
+				<v-col cols="12" sm="6" md="3" lg="2">
+					<v-text-field label="Search by name" v-model="filters.name" />
+				</v-col>
+				<v-col cols="12" sm="6" md="3" lg="2">
+					<v-text-field label="Search by file type" v-model="filters.file_type" />
+				</v-col>
+				<v-col cols="12" sm="6" md="4" lg="3">
+					<DateRangeSelect
+						:filters="filters"
+						start-key="created_start"
+						end-key="created_end"
+						label="Created Date Range"
+					/>
+				</v-col>
+			</v-row>
+		</template>
+		
+		<template #cards-slot="{ items }">
+			<v-row>
+				<v-col cols="12" md="4" lg="3" v-for="item in items" :key="item.id">
+					<v-card height="100%" link 
+						:to="{ name: 'Attachment', params: { attachmentId: item.id } }" 
+						variant="flat" class="border">
+						<v-img :src="item.file" max-height="200" contain></v-img>
+						<v-card-title class="text-subtitle-1">{{ item.name }}</v-card-title>
+						<v-card-subtitle>{{ item.file_type }}</v-card-subtitle>
+						<v-card-text>
+							<v-chip color="primary">{{ formatDateTime(item.created_at) }}</v-chip>
+						</v-card-text>
+					</v-card>
+				</v-col>
+			</v-row>
+		</template>
+	</ResponsiveDataTable>
+</template>
+
+<script setup>
+import {
+	attachmentDefaultHeaders,
+} from "../config";
+import { getAttachments } from "../api";
+import ResponsiveDataTable from "@/components/ResponsiveDataTable.vue";
+import DateRangeSelect from "@/components/DateRangeSelect.vue";
+import { formatDateTime } from "@/services/utils";
+import { ref } from "vue";
+
+const props = defineProps({
+	title: {
+		type: String,
+		default: "Media",
+	},
+	subtitle: {
+		type: String,
+		default: "Click on any tile to view or delete",
+	},
+});
+
+// Use a simple ref for filters like other components
+const filters = ref({});
+</script>
--- src/apps/attachments/views/AttachmentsPage.vue
+++ src/apps/attachments/views/AttachmentsPage.vue
@@ -1,78 +1,9 @@
 <template>
-	<v-card>
-		<v-card-title>
-			{{ title }}
-		</v-card-title>
-		<v-card-subtitle>
-			{{ subtitle }}
-		</v-card-subtitle>
-
-		<v-row> 
-			<v-col>
-				<StatsCard :fetchStats="getAttachmentStats" title="Attachments" />
-			</v-col>
-		</v-row>
-		<v-row>
-			<v-col>
-				<FilterCard :fields="fields" />
-			</v-col>
-		</v-row>
-		<v-row>
-			<ResponsiveDataTable 
-				:getToFunction="(item) => ({ name: 'Attachment', params: { attachmentId: item.id } })"
-				:headers="attachmentDefaultHeaders" 
-				:fetch="getAttachments" 
-				v-model:filters="filters" 
-				:desktopTemplate="props.overrideDesktopTemplate"
-				mobileTemplate="card" />
-		</v-row>
-	</v-card>
+	<v-container>
+		<AttachmentsLookup />
+	</v-container>
 </template>
 
 <script setup>
-import FilterCard from "@/components/FilterCard.vue";
-import {
-	attachmentDefaultFilterFields,
-	attachmentDefaultHeaders,
-} from "../config";
-import { getAttachments, getAttachmentStats } from "../api";
-import ResponsiveDataTable from "@/components/ResponsiveDataTable.vue";
-import StatsCard from "@/components/StatsCard.vue";
-
-import { computed, ref } from "vue";
-
-const props = defineProps({
-	title: {
-		type: String,
-		default: "Media",
-	},
-	subtitle: {
-		type: String,
-		default: "Click on any tile to view or delete",
-	},
-	overrideFields: {
-		type: Array,
-	},
-	overrideDesktopTemplate: {
-		type: String,
-		default: "card",
-	},
-});
-
-const fields = ref(
-	props.overrideFields ? props.overrideFields : attachmentDefaultFilterFields,
-);
-
-const filters = computed(() => {
-	return fields.value.reduce((acc, field) => {
-		if (Array.isArray(field.key)) {
-			field.key.forEach((k, i) => {
-				acc[k] = field.value?.[i] ?? null;
-			});
-		} else {
-			acc[field.key] = field.value;
-		}
-		return acc;
-	}, {});
-});
+import AttachmentsLookup from "../components/AttachmentsLookup.vue";
 </script>
