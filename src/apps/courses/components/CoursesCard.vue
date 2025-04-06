8441b08cbe5571b20241316881a55fbbb3968a6f
Raj Tagore <rajtagore780@gmail.com>
2025-04-06 17:16:54 +0530

    --

--- src/apps/courses/components/CoursesCard.vue
+++ src/apps/courses/components/CoursesCard.vue
@@ -1,32 +1,27 @@
 <template>
-    <v-card>
-        <v-card-title>
-            {{ title }}
-        </v-card-title>
-        <v-card-subtitle>
-            {{ subtitle }}
-        </v-card-subtitle>
-        <ResponsiveDataTable 
-            :fetch="getCourses" 
-            v-model:filters="props.filters"
-			:templates="{ desktop: 'card', mobile: 'card' }"
-            forceMobile>
-            <template #cards-slot="{ items }">
-                <v-row>
-                    <v-col cols="12" md="4" lg="3" v-for="item in items" :key="item.id">
-                        <v-card class="border" variant="flat" :to="{name: 'Course', params: {courseId: item.id}}">
-                            <v-card-title class="text-subtitle-1">
-                                {{ item.name }}
-                            </v-card-title>
-                            <v-card-subtitle>
-                                {{ item.code }}
-                            </v-card-subtitle>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </template>
-        </ResponsiveDataTable>
-    </v-card>
+    <ResponsiveDataTable 
+        :title="title"
+        :subtitle="subtitle"
+        :fetch="getCourses" 
+        v-model:filters="props.filters"
+        :templates="{ desktop: 'card', mobile: 'card' }"
+        forceMobile
+        hideFilters>
+        <template #cards-slot="{ items }">
+            <v-row>
+                <v-col cols="12" md="4" lg="3" v-for="item in items" :key="item.id">
+                    <v-card class="border" variant="flat" :to="{name: 'Course', params: {courseId: item.id}}">
+                        <v-card-title class="text-subtitle-1">
+                            {{ item.name }}
+                        </v-card-title>
+                        <v-card-subtitle>
+                            {{ item.code }}
+                        </v-card-subtitle>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </template>
+    </ResponsiveDataTable>
 </template>
 
 <script setup>
