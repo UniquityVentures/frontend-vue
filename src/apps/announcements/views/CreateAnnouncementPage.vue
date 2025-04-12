14c712cf62ee6d6578b93c9b9013f57658d15e89
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-04-12 10:33:30 +0530

    Some minor things

--- src/apps/announcements/views/CreateAnnouncementPage.vue
+++ src/apps/announcements/views/CreateAnnouncementPage.vue
@@ -3,7 +3,7 @@
 		<AnnouncementForm
 			:announcement = "$route.query"
 			actionName="Create"
-			:action="createAnnouncement"
+			:action="handleCreate"
 		/>
 	</v-container>
 </template>
@@ -11,4 +11,13 @@
 <script setup>
 import { createAnnouncement } from "@/apps/announcements/api";
 import AnnouncementForm from "../components/AnnouncementForm.vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const handleCreate = async (data) => {
+	const resp = await createAnnouncement(data);
+	router.push({ name: "Announcement", params: { announcementId: resp.id } });
+	return resp;
+};
 </script>
