dbefdb49a0a26bd294172ec96077ec33917e2911
Raj Tagore <rajtagore780@gmail.com>
2025-05-01 16:34:52 +0530

    create and edit events

---
+++ src/apps/calendar/views/CreateEventPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <EventForm
+      :event="$route.query"
+      actionName="Create"
+      :action="handleCreate"
+      title="Create New Event"
+    />
+  </v-container>
+</template>
+
+<script setup>
+import { createEvent } from "@/apps/calendar/api";
+import EventForm from "../../calendar/components/EventForm.vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const handleCreate = async (data) => {
+  const resp = await createEvent(data);
+  router.push({ name: "Event", params: { eventId: resp.id } });
+  return resp;
+};
+</script> 
---
+++ src/apps/calendar/views/EditEventPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <EventForm
+      v-if="event"
+      actionName="Update"
+      :action="updateEvent"
+      :event="event"
+      title="Edit Event"
+    />
+  </v-container>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { getEvent, updateEvent } from "../api";
+import EventForm from "../../calendar/components/EventForm.vue";
+
+const props = defineProps({
+  eventId: {
+    type: Number,
+    required: true,
+  },
+});
+
+const event = ref(null);
+
+onMounted(async () => {
+  event.value = await getEvent(props.eventId);
+});
+</script>
